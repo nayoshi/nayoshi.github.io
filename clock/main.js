10307f9add328ffd19b412b358359beda2585488
Matthew Bautista <Nayoshi12@users.noreply.github.com>
2017-01-04 18:14:23 -0500

    Update main.js

--- clock/main.js
+++ clock/main.js
@@ -3,15 +3,20 @@ var g;
 var b;
 function setup(){
  createCanvas(windowWidth,windowHeight);
+ frameRate(1);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 function draw(){
+  b = second();
 	r = hour();
   g = minute();
-  b = second();
- background(r, g, b);
+
+  var rsec = random(255);
+  var gsec = random(255);
+  var bsec = random(255);
+ background(rsec, gsec, bsec);
  push();
  textSize(85);
  stroke(0);
